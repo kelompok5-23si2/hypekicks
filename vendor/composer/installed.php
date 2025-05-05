898a8ed03702efe4c84bb135ceee1c48b29da783
kelompok5-23si2 <project23si2@gmail.com>
2025-05-05 12:02:08 +0700

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'edww9/waystory_source_code',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'a87f23eb55f1e542c8856dd2b2f0d02b388bb60e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'edww9/waystory_source_code' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'a87f23eb55f1e542c8856dd2b2f0d02b388bb60e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.9.2',
+            'version' => '6.9.2.0',
+            'reference' => 'a7b17b42fa4887c92146243f3d2f4ccb962af17c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
